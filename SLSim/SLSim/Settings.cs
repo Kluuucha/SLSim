73f964c015222680a69a2ff394a8b975ac5f36c9
cord123123 <52894859+cord123123@users.noreply.github.com>
2020-04-17 00:41:59 +0200

    Added ProgramData Class

--- SLSim/SLSim/Settings.cs
+++ SLSim/SLSim/Settings.cs
@@ -3,12 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace SLSim
 {
-    class Settings
+    public class Settings
     {
         public static int xResolution = 1000;
         public static int yResolution = 500;
+    }
+    public class ProgramData
+    {
+        public static IList<Organism> organisms = new IList<Organism>();
+        public static IList<Pozywienie> food = new IList<Pozywienie>();
+       
     }
 }
---
+++ SLSim/SLSim/organism.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SLSim
+{
+    public class Organism
+    {
+        int x, y;
+        void fixPosition()
+        {
+            if (x < 0) x = 0;
+            else if (x > Settings.xResolution) x = Settings.xResolution - 1;
+            if (y < 0) x = 0;
+            else if (y > Settings.yResolution) y = Settings.yResolution - 1;
+        }
+        Organism(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+            fixPosition();
+            ProgramData.organisms.Add(this);
+        }
+    }
+}
