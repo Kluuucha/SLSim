70e6cde7aa5fe7af3b705194bfb2051b3bb5f727
cord123123 <52894859+cord123123@users.noreply.github.com>
2020-04-16 17:28:12 +0200

    Added Settings class

---
+++ SLSim/SLSim/Settings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SLSim
+{
+    class Settings
+    {
+        public static int xResolution = 1000;
+        public static int yResolution = 500;
+    }
+}
