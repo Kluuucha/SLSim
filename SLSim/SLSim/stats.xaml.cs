c0756289450b73ad6d0ce3ca0fd3e92e1d56f081
Grzegorz Rychlik <rychlikg97@gmail.com>
2020-05-24 12:19:38 +0200

    Statystyki ogólne

--- SLSim/SLSim/stats.xaml.cs
+++ SLSim/SLSim/stats.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
+using System.Security.Permissions;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,22 +21,72 @@ namespace SLSim
     /// </summary>
     public partial class stats: Window
     {
+        Organism pom;
+        int gat1 = 0;
+        int gat2 = 0;
+        int gat5 = 0;
+        int gat3 = 0;
+        int gat4 = 0;
+        int ileNaPlaszy = 0;
+
         public stats()
         {
             InitializeComponent();
-            int iorg = Simulation.simulationGrid.Where(kpv => kpv.Value is Organism).Count();
-            org.Text = iorg.ToString();
-            int ifd = Simulation.simulationGrid.Where(kpv => kpv.Value is Food).Count();
-            fd.Text = ifd.ToString();
 
-           // fd_Copy.Text = Settings.breedingChance.ToString();
-            fd_Copy1.Text = Settings.stepsPerTic.ToString();
-            fd_Copy2.Text = Settings.elementSize.ToString();
+            if (Settings.numberOfSpecies == 1)
+            {
+                dwa.Visibility = Visibility.Hidden;
+                trzy.Visibility = Visibility.Hidden;
+                cztery.Visibility = Visibility.Hidden;
+                piec.Visibility = Visibility.Hidden;
+            }
+            else if (Settings.numberOfSpecies == 2)
+            {
+                trzy.Visibility = Visibility.Hidden;
+                cztery.Visibility = Visibility.Hidden;
+                piec.Visibility = Visibility.Hidden;
+            }
+            else if (Settings.numberOfSpecies == 3)
+            {
+                cztery.Visibility = Visibility.Hidden;
+                piec.Visibility = Visibility.Hidden;
+            }
+            else if (Settings.numberOfSpecies == 4)
+            {
+                piec.Visibility = Visibility.Hidden;
+            }
+
+
+            iloscOrganizmow.Text = Simulation.simulationGrid.Where(kpv => kpv.Value is Organism).Count().ToString();
+            iloscPozywienia.Text = Simulation.simulationGrid.Where(kpv => kpv.Value is Food).Count().ToString();
+            iloscGatonkowPocz.Text = Settings.numberOfSpecies.ToString();
+            krokiNaTick.Text = Settings.stepsPerTic.ToString();
+
+
+            foreach (KeyValuePair<int, SimObject> kvp in Simulation.simulationGrid)
+            {
+                if (kvp.Value is Organism)
+                {
+                    pom = (Organism)kvp.Value;
+                    if (pom.species.Equals(Simulation.speciesList[0])) gat1++;
+                    if (pom.species.Equals(Simulation.speciesList[1])) gat2++;
+                    if (pom.species.Equals(Simulation.speciesList[2])) gat3++;
+                    if (pom.species.Equals(Simulation.speciesList[3])) gat4++;
+                    if (pom.species.Equals(Simulation.speciesList[4])) gat5++;
+                }
+            }
+            if (gat1 > 0) ileNaPlaszy++;
+            if (gat2 > 0) ileNaPlaszy++;
+            if (gat3 > 0) ileNaPlaszy++;
+            if (gat4 > 0) ileNaPlaszy++;
+            if (gat5 > 0) ileNaPlaszy++;
+            iloscGatonkowNaPlanszy.Text = ileNaPlaszy.ToString();
         }
-
+       
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             Close();
         }
+
     }
 }
